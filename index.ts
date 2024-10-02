c77ac0db6e183c3aa8e8aa3656b9db9bebe2063e
Serpentariu13 <serpentarium13@mail.ru>
2024-10-02 15:42:12 +0300

    Add express

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-Bun.serve({
-  port: 3000,
-  fetch(req) {
-    return new Response("Bun!");
-  },
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
